90b9a39e31553c1d5c06975ac32da995770ab028
Laura <laumquispe@gmail.com>
2023-04-16 19:14:47 -0300

    modificacion edicion cajageneral

--- app/serializers/cajageneral_serializer.rb
+++ app/serializers/cajageneral_serializer.rb
@@ -1,6 +1,6 @@
 class CajageneralSerializer < ActiveModel::Serializer
   attributes :id, :fecha, :nrocomprobante, :nroordenpago, :importe, :observacion, :activo, :tiporegistro_id, :createdregistro,:tipocomprobante_id,
-  :proveedor_id, :formapago_id, :saldo
+  :proveedor_id, :formapago_id, :saldo, :concepto_id,:subconcepto_id,
   :updatedregistro
   has_one :tiporegistro
   has_one :concepto
